7c867534038a65bf9a711987cf2fdebb3f5bb4cf
Mark Peek <markpeek@vmware.com>
2019-03-27 22:03:27 -0700

    Add some intiial cas resources and data sources
    - Add cas_cloud_account_aws data source and resource
    - Add cas_project resource
    - Add cas_region data source
    - Add cas_zone resource

---
+++ cas/resource_cloud_account_aws.go
@@ -0,0 +1,178 @@
+package cas
+
+import (
+	"fmt"
+
+	"github.com/vmware/cas-sdk-go/pkg/client/cloud_account"
+	"github.com/vmware/cas-sdk-go/pkg/models"
+	tango "github.com/vmware/terraform-provider-cas/sdk"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceCloudAccountAWS() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudAccountAWSCreate,
+		Read:   resourceCloudAccountAWSRead,
+		Update: resourceCloudAccountAWSUpdate,
+		Delete: resourceCloudAccountAWSDelete,
+
+		Schema: map[string]*schema.Schema{
+			"access_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"regions": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"region_ids": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"secret_key": &schema.Schema{
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func resourceCloudAccountAWSCreate(d *schema.ResourceData, m interface{}) error {
+	var regions []string
+
+	client := m.(*tango.Client)
+	apiClient := client.GetAPIClient()
+
+	accessKey := d.Get("access_key").(string)
+	description := d.Get("description").(string)
+	name := d.Get("name").(string)
+	secretAccessKey := d.Get("secret_key").(string)
+
+	if v, ok := d.GetOk("regions"); ok {
+		if !compareUnique(v.([]interface{})) {
+			return fmt.Errorf("Specified regions are not unique")
+		}
+		regions = expandStringList(v.([]interface{}))
+	}
+
+	createResp, err := apiClient.CloudAccount.CreateAwsCloudAccount(cloud_account.NewCreateAwsCloudAccountParams().WithBody(&models.CloudAccountAwsSpecification{
+		AccessKeyID:        &accessKey,
+		CreateDefaultZones: false,
+		Description:        description,
+		Name:               &name,
+		SecretAccessKey:    &secretAccessKey,
+		RegionIds:          regions,
+	}))
+
+	if err != nil {
+		return err
+	}
+
+	// The returned EnabledRegionIds and Hrefs containing the region ids can be in a different order than the request order.
+	// Call a routine to normalize the order to correspond with the users region order.
+	regionsIds, err := flattenAndNormalizeCLoudAccountRegionIds(regions, createResp.Payload)
+	if err != nil {
+		return err
+	}
+	d.Set("region_ids", regionsIds)
+	d.SetId(*createResp.Payload.ID)
+
+	return resourceCloudAccountAWSRead(d, m)
+}
+
+func resourceCloudAccountAWSRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*tango.Client)
+	apiClient := client.GetAPIClient()
+
+	id := d.Id()
+	ret, err := apiClient.CloudAccount.GetAwsCloudAccount(cloud_account.NewGetAwsCloudAccountParams().WithID(id))
+	if err != nil {
+		switch err.(type) {
+		case *cloud_account.GetAwsCloudAccountNotFound:
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+	awsAccount := *ret.Payload
+
+	d.Set("access_key", awsAccount.AccessKeyID)
+	d.Set("description", awsAccount.Description)
+	d.Set("name", awsAccount.Name)
+	regions := awsAccount.EnabledRegionIds
+	d.Set("regions", regions)
+
+	// The returned EnabledRegionIds and Hrefs containing the region ids can be in a different order than the request order.
+	// Call a routine to normalize the order to correspond with the users region order.
+	regionsIds, err := flattenAndNormalizeCLoudAccountAWSRegionIds(regions, &awsAccount)
+	if err != nil {
+		return err
+	}
+	d.Set("region_ids", regionsIds)
+
+	return nil
+}
+
+func resourceCloudAccountAWSUpdate(d *schema.ResourceData, m interface{}) error {
+	var regions []string
+
+	client := m.(*tango.Client)
+	apiClient := client.GetAPIClient()
+
+	id := d.Id()
+	accessKey := d.Get("access_key").(string)
+	description := d.Get("description").(string)
+	name := d.Get("name").(string)
+	secretAccessKey := d.Get("secret_key").(string)
+
+	if v, ok := d.GetOk("regions"); ok {
+		if !compareUnique(v.([]interface{})) {
+			return fmt.Errorf("Specified regions are not unique")
+		}
+		regions = expandStringList(v.([]interface{}))
+	}
+	_, err := apiClient.CloudAccount.UpdateCloudAccount(cloud_account.NewUpdateCloudAccountParams().WithID(id).WithBody(&models.CloudAccountSpecification{
+		PrivateKeyID:       &accessKey,
+		CreateDefaultZones: false,
+		Description:        description,
+		Name:               &name,
+		PrivateKey:         &secretAccessKey,
+		RegionIds:          regions,
+	}))
+	if err != nil {
+		return err
+	}
+
+	return resourceCloudAccountAWSRead(d, m)
+}
+
+func resourceCloudAccountAWSDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*tango.Client)
+	apiClient := client.GetAPIClient()
+
+	id := d.Id()
+	_, err := apiClient.CloudAccount.DeleteAwsCloudAccount(cloud_account.NewDeleteAwsCloudAccountParams().WithID(id))
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
--- cas/tags.go
+++ cas/tags.go
@@ -1,9 +1,10 @@
 package cas
 
 import (
-	"github.com/vmware/terraform-provider-cas/sdk"
+	tango "github.com/vmware/terraform-provider-cas/sdk"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/vmware/cas-sdk-go/pkg/models"
 )
 
 // tagsSchema returns the schema to use for the tags property
@@ -45,6 +46,62 @@ func expandTags(configTags []interface{}) []tango.Tag {
 }
 
 func flattenTags(tags []tango.Tag) []interface{} {
+	if len(tags) == 0 {
+		return make([]interface{}, 0)
+	}
+
+	configTags := make([]interface{}, 0, len(tags))
+
+	for _, tag := range tags {
+		helper := make(map[string]interface{})
+		helper["key"] = tag.Key
+		helper["value"] = tag.Value
+
+		configTags = append(configTags, helper)
+	}
+
+	return configTags
+}
+
+// tagsSchema returns the schema to use for the tags property
+func tagsSDKSchema() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeSet,
+		Optional: true,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"key": {
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"value": {
+					Type:     schema.TypeString,
+					Required: true,
+				},
+			},
+		},
+	}
+}
+
+func expandSDKTags(configTags []interface{}) []*models.Tag {
+	tags := make([]*models.Tag, 0, len(configTags))
+
+	for _, configTag := range configTags {
+		tagMap := configTag.(map[string]interface{})
+
+		tag := models.Tag{
+			Key:   withString(tagMap["key"].(string)),
+			Value: withString(tagMap["value"].(string)),
+		}
+
+		tags = append(tags, &tag)
+	}
+
+	return tags
+}
+
+func flattenSDKTags(tags []*models.Tag) []interface{} {
 	if len(tags) == 0 {
 		return make([]interface{}, 0)
 	}
