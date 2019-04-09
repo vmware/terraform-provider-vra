8537d87c6b86261b6f64ecae0a5f12a345b93ddc
Mark Peek <mark@peek.org>
2019-04-09 16:02:32 -0700

    Add cas_image data source (#9)

---
+++ cas/data_source_image.go
@@ -0,0 +1,79 @@
+package cas
+
+import (
+	"fmt"
+
+	"github.com/vmware/cas-sdk-go/pkg/client/fabric_images"
+	tango "github.com/vmware/terraform-provider-cas/sdk"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceImage() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceImageRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"external_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"private": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"region": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceImageRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*tango.Client)
+	apiClient := client.GetAPIClient()
+
+	filter := d.Get("filter").(string)
+
+	getResp, err := apiClient.FabricImages.GetFabricImages(fabric_images.NewGetFabricImagesParams().WithDollarFilter(withString(filter)))
+	if err != nil {
+		return err
+	}
+
+	images := getResp.Payload
+	if len(images.Content) > 1 {
+		return fmt.Errorf("cas_image must filter to a single image")
+	}
+	if len(images.Content) == 0 {
+		return fmt.Errorf("cas_image filter did not match any images")
+	}
+
+	image := images.Content[0]
+	d.Set("description", image.Description)
+	d.Set("external_id", image.ExternalID)
+	d.Set("id", image.ID)
+	d.Set("name", image.Name)
+	d.Set("private", image.IsPrivate)
+	d.Set("region", image.ExternalRegionID)
+
+	d.SetId(*image.ID)
+
+	return nil
+}
