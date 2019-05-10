66a319ca6b324fb62a5852edd6f052f9f1a9c2f1
Prativa Bawri <bawrip@vmware.com>
2019-05-09 17:22:24 -0700

    Adding new resource: resource_block_device (#23)
    * Adding new resource: resource_block_device
    
    * Adding an end-to-end example for creating aws cloud account and the other resources
    
    Note: Will keep adding to this example as new resources are implemented

---
+++ examples/cloud_account_aws/main.tf
@@ -0,0 +1,77 @@
+provider "cas" {
+    url = "${var.url}"
+    refresh_token = "${var.refresh_token}"
+}
+
+resource "cas_cloud_account_aws" "my-cloud-account" {
+	name = "tf-cas-cloud-account-aws"
+	description = "my terraform test cloud account aws"
+	access_key = "${var.access_key}"
+	secret_key = "${var.secret_key}"
+	regions = ["us-east-1", "us-west-1"]
+ }
+
+ data "cas_region" "us-east-1-region" {
+    cloud_account_id = "${cas_cloud_account_aws.my-cloud-account.id}"
+    region = "us-east-1"
+}
+
+ resource "cas_zone" "my-zone" {
+    name = "tf-cas-zone"
+    description = "my terraform test cloud zone"
+    region_id = "${data.cas_region.us-east-1-region.id}"
+    tags {
+        key = "my-tf-key"
+        value = "my-tf-value"
+    }
+    tags {
+        key = "tf-foo"
+        value = "tf-bar"
+    }
+}
+
+resource "cas_project" "my-project" {
+    name = "tf-cas-project"
+    description = "my terraform test project"
+    zone_assignments {
+        zone_id = "${cas_zone.my-zone.id}"
+        priority = 1
+        max_instances = 2
+    }
+}
+
+resource "cas_flavor" "my-flavor" {
+	name = "tf-cas-flavor"
+	description = "my terraform test flavor"
+	region_id = "${data.cas_region.us-east-1-region.id}"
+	flavor_mapping {
+		name = "small"
+		instance_type = "t2.small"
+	}
+	flavor_mapping {
+		name = "medium"
+		instance_type = "t2.medium"
+	}
+}
+
+resource "cas_image_profile" "my-image-profile" {
+    name = "tf-cas-image-profile"
+    description = "my terraform test image profile"
+    region_id = "${data.cas_region.us-east-1-region.id}"
+    image_mapping {
+        name = "ubuntu"
+        image_name = "${var.image_name}"
+    }
+}
+
+resource "cas_block_device" "my-block-device" {
+    name = "tf-cas-block-device"
+    capacity_in_gb = 4
+    project_id = "${cas_project.my-project.id}"
+
+    tags {
+        key = "tf-foo"
+        value = "tf-bar"
+    }
+}
+
