20d2751d6c3a71d21821f2ce23dcef55ce66b7ff
Mark Peek <markpeek@vmware.com>
2019-10-25 10:39:46 -0700

    Add script to release binaries to github
    Signed-off-by: Mark Peek <markpeek@vmware.com>

---
+++ scripts/release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+COMMANDS="gox github-release"
+NAME=terraform-provider-vra
+NOWDATE=$(TZ=UTC date +%FT%T%Z)
+VERSION=$(git describe --tags --always)
+VERSION_LONG=$(git describe --tags --long --always)
+
+check_dependencies() {
+    deps=$1
+    for cmd in $deps; do
+        which $cmd > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Command $cmd not found, please install it."
+            exit 1
+        fi
+    done
+}
+
+build_providers() {
+    rm -rf build
+    mkdir build
+    GO111MODULE=on CGO_ENABLED=0 gox -ldflags "-X main.version=${VERSION} -X main.buildTime=${NOWDATE}" \
+        -os "linux darwin windows" -arch "amd64" -output "build/{{.OS}}_{{.Arch}}/terraform-provider-vra"
+    if [ $? -ne 0 ]; then
+        echo "Error building providers...exiting"
+        exit 1
+    fi
+}
+
+package(){
+    rm -rf release
+    mkdir release
+    for f in build/*; do \
+        g=`basename $f`; \
+        tar -zcf release/${NAME}-${g}-${VERSION}.tgz -C build/${g} .; \
+    done
+}
+
+release(){
+	github-release release \
+		--user vmware \
+		--repo "${NAME}" \
+		--target "$(git rev-parse --abbrev-ref HEAD)" \
+		--tag "${VERSION}" \
+		--name "${VERSION}"
+	cd release && ls *.tgz | xargs -I FILE github-release upload \
+		--user vmware \
+		--repo "${NAME}" \
+		--tag "${VERSION}" \
+		--name FILE \
+		--file FILE
+}
+
+check_dependencies "$COMMANDS"
+build_providers
+package
+release
