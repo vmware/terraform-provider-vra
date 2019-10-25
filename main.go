6204f892deddf0915e8b45989ee729d8d681395c
Mark Peek <markpeek@vmware.com>
2019-10-25 11:29:31 -0700

    Add script to release binaries to github (#85)
    Signed-off-by: Mark Peek <markpeek@vmware.com>

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/vmware/terraform-provider-vra/vra"
 
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
+var (
+	buildTime string
+	version   string
+)
+
 func main() {
+	versionPtr := flag.Bool("v", false, "show version info")
+	flag.Parse()
+
+	if *versionPtr {
+		fmt.Printf("version: %s\n", version)
+		fmt.Printf("build time: %s\n", buildTime)
+		os.Exit(0)
+	}
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
 			return vra.Provider()
