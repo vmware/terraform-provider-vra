98d9d2cba916a7143bfecc3dafe6799de69db108
Deepak Mettem <dmettem@vmware.com>
2020-04-07 11:23:51 -0700

    Add output samples in deployment resource examples
    Signed-off-by: Deepak Mettem <dmettem@vmware.com>

--- examples/deployment/blueprint/main.tf
+++ examples/deployment/blueprint/main.tf
@@ -25,7 +25,50 @@ resource "vra_deployment" "this" {
     count  = 2
     flag   = true
   }
+}
 
-  expand_resources    = true
-  expand_last_request = true
+
+output "resources" {
+  description = "All the resources from a vRA deployment"
+  value       = vra_deployment.this.resources
+}
+
+output "resource_properties_by_name" {
+  description = "Properties of all resources by its name from a vRA deployment"
+  value = {
+    for rs in vra_deployment.this.resources :
+    rs.name => jsondecode(rs.properties_json)
+  }
+}
+
+output "resources_properties" {
+  description = "Properties of all resources from a vRA deployment"
+  value = [
+    for rs in vra_deployment.this.resources :
+    jsondecode(rs.properties_json)
+  ]
+}
+
+output "addresses_by_name" {
+  description = "Resource name and IP addresses of all machine type resources from a vRA deployment"
+  value = {
+    for props in sort(vra_deployment.this.resources.*.properties_json) :
+    jsondecode(props).resourceName => jsondecode(props).address
+    if jsondecode(props).componentType == "Cloud.Machine" || jsondecode(props).componentType == "Cloud.vSphere.Machine" || jsondecode(props).componentType == "Cloud.AWS.EC2.Instance" || jsondecode(props).componentType == "Cloud.GCP.Machine" || jsondecode(props).componentType == "Cloud.Azure.Machine"
+  }
+}
+
+output "first_resource_address" {
+  // Works for simple deployments with only one machine resource with count as 1.
+  description = "IP address property of first resource in a vRA deployment"
+  value       = jsondecode(sort(vra_deployment.this.resources.*.properties_json)[0]).address
+}
+
+output "all_resources_addresses" {
+  // Gives address property from all resources of a deployment
+  description = "IP address property of all resources from a vRA deployment"
+  value = [
+    for props in sort(vra_deployment.this.resources.*.properties_json) :
+    jsondecode(props).address
+  ]
 }
