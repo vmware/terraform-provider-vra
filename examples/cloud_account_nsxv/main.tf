463817dcd3e9099f27f203794ac8ed8d352be99b
Deepak Mettem <dmettem@vmware.com>
2020-07-28 15:37:47 -0700

    Make data collector id optional for NSX-T/V cloud accounts (#221)
    For creating a NSX-T/V cloud account, one doesn't need data collector
    (dcId) with vRA 8.x. It is required only for vRA Cloud.
    
    This commit makes the dcId as optional in schema for NSX-T/V cloud
    account resources.
    
    Signed-off-by: Deepak Mettem <dmettem@vmware.com>

--- examples/cloud_account_nsxv/main.tf
+++ examples/cloud_account_nsxv/main.tf
@@ -1,10 +1,12 @@
 provider "vra" {
   url           = var.url
   refresh_token = var.refresh_token
+  insecure      = var.insecure
 }
 
 data "vra_data_collector" "dc" {
-  name = var.datacollector
+  count = var.cloud_proxy != "" ? 1 : 0
+  name  = var.cloud_proxy
 }
 
 resource "vra_cloud_account_nsxv" "this" {
@@ -13,7 +15,7 @@ resource "vra_cloud_account_nsxv" "this" {
   username    = var.username
   password    = var.password
   hostname    = var.hostname
-  dc_id        = data.vra_data_collector.dc.id
+  dc_id       = var.cloud_proxy != "" ? data.vra_data_collector.dc[0].id : ""
 
   accept_self_signed_cert = true
 
