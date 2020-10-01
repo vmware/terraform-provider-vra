c903f67d8a32af0643fa845c393d34eafb7265d2
Deepak Mettem <dmettem@vmware.com>
2020-09-30 23:31:50 -0700

    Add data source for fabric Azure storage account
    Signed-off-by: Deepak Mettem <dmettem@vmware.com>

---
+++ vra/data_source_fabric_storage_account_azure.go
@@ -0,0 +1,144 @@
+package vra
+
+import (
+	"github.com/vmware/vra-sdk-go/pkg/client/fabric_azure_storage_account"
+	"github.com/vmware/vra-sdk-go/pkg/models"
+
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceFabricStorageAccountAzure() *schema.Resource {
+	return &schema.Resource{
+		Read: resourceFabricStorageAccountAzureRead,
+
+		Schema: map[string]*schema.Schema{
+			"cloud_account_ids": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "Set of ids of the cloud accounts this entity belongs to.",
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date when the entity was created. The date is in ISO 8601 and UTC.",
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A human-friendly description of the fabric Azure storage account.",
+			},
+			"external_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "External entity Id on the provider side.",
+			},
+			"external_region_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Indicates the ID of region.",
+			},
+			"filter": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"id"},
+				Description:   "Search criteria to narrow down the fabric Azure storage accounts.",
+			},
+			"id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"filter"},
+				Description:   "The id of this fabric Azure storage account.",
+			},
+			"links": linksSchema(),
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A human-friendly name of the fabric Azure storage account.",
+			},
+			"org_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The id of the organization this entity belongs to.",
+			},
+			"owner": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Email of the user that owns the entity.",
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Indicates the performance tier for the storage type. Premium disks are SSD backed and Standard disks are HDD backed. example: Standard_LRS / Premium_LRS",
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date when the entity was last updated. The date is ISO 8601 and UTC.",
+			},
+		},
+	}
+}
+
+func resourceFabricStorageAccountAzureRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("Reading the vra_fabric_storage_account_azure data source with filter [%s]", d.Get("filter"))
+	apiClient := meta.(*Client).apiClient
+
+	var fabricAzureStorageAccount *models.FabricAzureStorageAccount
+
+	id := d.Get("id").(string)
+	filter := d.Get("filter").(string)
+
+	if id == "" && filter == "" {
+		return fmt.Errorf("one of id or filter is required")
+	}
+
+	if id != "" {
+		getResp, err := apiClient.FabricAzureStorageAccount.GetFabricAzureStorageAccount(fabric_azure_storage_account.NewGetFabricAzureStorageAccountParams().WithID(id))
+
+		if err != nil {
+			return err
+		}
+		fabricAzureStorageAccount = getResp.GetPayload()
+	} else {
+		getResp, err := apiClient.FabricAzureStorageAccount.GetFabricAzureStorageAccounts(fabric_azure_storage_account.NewGetFabricAzureStorageAccountsParams().WithDollarFilter(withString(filter)))
+		if err != nil {
+			return err
+		}
+
+		fabricAzureStorageAccounts := *getResp.Payload
+		if len(fabricAzureStorageAccounts.Content) > 1 {
+			return fmt.Errorf("vra_fabric_storage_account_azure must filter to a fabric Azure storage account")
+		}
+		if len(fabricAzureStorageAccounts.Content) == 0 {
+			return fmt.Errorf("vra_fabric_storage_account_azure filter did not match any fabric Azure storage accounts")
+		}
+
+		fabricAzureStorageAccount = fabricAzureStorageAccounts.Content[0]
+	}
+
+	d.SetId(*fabricAzureStorageAccount.ID)
+	d.Set("cloud_account_ids", fabricAzureStorageAccount.CloudAccountIds)
+	d.Set("created_at", fabricAzureStorageAccount.CreatedAt)
+	d.Set("description", fabricAzureStorageAccount.Description)
+	d.Set("external_id", fabricAzureStorageAccount.ExternalID)
+	d.Set("external_region_id", fabricAzureStorageAccount.ExternalRegionID)
+	d.Set("name", fabricAzureStorageAccount.Name)
+	d.Set("org_id", fabricAzureStorageAccount.OrgID)
+	d.Set("owner", fabricAzureStorageAccount.Owner)
+	d.Set("type", fabricAzureStorageAccount.Type)
+	d.Set("updated_at", fabricAzureStorageAccount.UpdatedAt)
+
+	if err := d.Set("links", flattenLinks(fabricAzureStorageAccount.Links)); err != nil {
+		return fmt.Errorf("error getting fabric Azure storage account links - error: %#v", err)
+	}
+
+	log.Printf("Finished reading the vra_fabric_storage_account_azure data source with filter [%s]", d.Get("filter"))
+	return nil
+}
---
+++ vra/data_source_fabric_storage_account_azure_test.go
@@ -0,0 +1,74 @@
+package vra
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceVRAFabricStorageAccountAzure(t *testing.T) {
+	dataSourceName := "data.vra_fabric_storage_account_azure.this"
+	regionName := os.Getenv("VRA_ARM_REGION_NAME")
+	fabricStorageAccountName := os.Getenv("VRA_ARM_FABRIC_STORAGE_ACCOUNT_NAME")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckFabricStorageAccountAzure(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceVRAFabricStorageAccountAzureNoConfig(),
+				ExpectError: regexp.MustCompile("one of id or filter is required"),
+			},
+			{
+				Config:      testAccDataSourceVRAFabricStorageAccountAzureNoneConfig(),
+				ExpectError: regexp.MustCompile("vra_fabric_storage_account_azure filter did not match any fabric Azure storage accounts"),
+			},
+			{
+				Config: testAccDataSourceVRAFabricStorageAccountOneConfig(regionName, fabricStorageAccountName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "external_region_id", regionName),
+					resource.TestCheckResourceAttr(dataSourceName, "name", fabricStorageAccountName),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_account_ids.#", "1"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "external_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "id"),
+					resource.TestCheckResourceAttr(dataSourceName, "links.#", "3"), // One each for self, cloud account and region
+					resource.TestCheckResourceAttrSet(dataSourceName, "org_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "type"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceVRAFabricStorageAccountAzureBaseConfig() string {
+	// Need valid credentials since this is creating a real cloud account
+	cloudAccountName := os.Getenv("VRA_ARM_CLOUD_ACCOUNT_NAME")
+	return fmt.Sprintf(`
+	data "vra_cloud_account_azure" "this" {
+		name = "%s"
+	}`, cloudAccountName)
+}
+
+func testAccDataSourceVRAFabricStorageAccountAzureNoConfig() string {
+	return testAccDataSourceVRAFabricStorageAccountAzureBaseConfig() + `
+	data "vra_fabric_storage_account_azure" "this" {
+	}`
+}
+
+func testAccDataSourceVRAFabricStorageAccountAzureNoneConfig() string {
+	return testAccDataSourceVRAFabricStorageAccountAzureBaseConfig() + `
+	data "vra_fabric_storage_account_azure" "this" {
+		filter = "name eq 'foobar'"
+	}`
+}
+
+func testAccDataSourceVRAFabricStorageAccountOneConfig(regionName, fabricStorageAccountName string) string {
+	return testAccDataSourceVRAFabricStorageAccountAzureBaseConfig() + fmt.Sprintf(`
+	data "vra_fabric_storage_account_azure" "this" {
+		filter = "name eq '%s' and externalRegionId eq '%s' and cloudAccountId eq '${data.vra_cloud_account_azure.this.id}'"
+	}`, fabricStorageAccountName, regionName)
+}
