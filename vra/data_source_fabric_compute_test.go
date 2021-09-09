b7d4bba1afdcda3cd27cf627023017daba7a3dc0
Ferran Rodenas <rodenasf@vmware.com>
2021-09-09 12:11:21 -0700

    Add fabric_compute data source
    Signed-off-by: Ferran Rodenas <rodenasf@vmware.com>

---
+++ vra/data_source_fabric_compute_test.go
@@ -0,0 +1,57 @@
+package vra
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccFabricCompute_Basic(t *testing.T) {
+	regionName := os.Getenv("VRA_FABRIC_COMPUTE_NAME")
+	dataSourceName := "data.vra_fabric_compute.my-fabric-compute"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckFabricCompute(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataSourceVRAFabricComputeNoConfig(),
+				ExpectError: regexp.MustCompile("one of id or filter is required"),
+			},
+			{
+				Config:      testAccDataSourceVRAFabricComputeNoneConfig(),
+				ExpectError: regexp.MustCompile("filter doesn't match to any fabric compute"),
+			},
+			{
+				Config: testAccDataSourceVRAFabricComputeOneConfig(regionName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "external_id", regionName),
+					resource.TestCheckResourceAttr(dataSourceName, "name", regionName),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceVRAFabricComputeNoConfig() string {
+	return `
+		data "vra_fabric_compute" "my-fabric-compute" {
+		}`
+}
+
+func testAccDataSourceVRAFabricComputeNoneConfig() string {
+	return `
+		data "vra_fabric_compute" "my-fabric-compute" {
+			filter = "name eq 'foobar'"
+		}`
+}
+
+func testAccDataSourceVRAFabricComputeOneConfig(regionName string) string {
+	return fmt.Sprintf(`
+		data "vra_fabric_compute" "my-fabric-compute" {
+			filter = "name eq '%s'"
+		}`, regionName)
+}
