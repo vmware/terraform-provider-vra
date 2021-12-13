81e382a6310f64da6fa8f61490c0b075ec1a89c6
Amakou M <mars4life@gmail.com>
2021-12-13 11:01:21 +0100

    Add new resource catalog_item_entitlement
    Signed-off-by: Amakou MBATA <mars4life@gmail.com>

---
+++ vra/resource_catalog_item_entitlement.go
@@ -0,0 +1,139 @@
+package vra
+
+import (
+        "github.com/go-openapi/strfmt"
+        "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+        "github.com/vmware/vra-sdk-go/pkg/client/catalog_entitlements"
+        "github.com/vmware/vra-sdk-go/pkg/models"
+
+        "log"
+)
+
+func resourceCatalogItemEntitlement() *schema.Resource {
+        return &schema.Resource{
+                Create: resourceCatalogItemEntitlementCreate,
+                Delete: resourceCatalogItemEntitlementDelete,
+                Read:   resourceCatalogItemEntitlementRead,
+                Importer: &schema.ResourceImporter{
+                        State: schema.ImportStatePassthrough,
+                },
+
+                Schema: map[string]*schema.Schema{
+                        "catalog_item_id": {
+                                Type:     schema.TypeString,
+                                Required: true,
+                                ForceNew: true,
+                        },
+                        "definition": {
+                                Type:     schema.TypeSet,
+                                Computed: true,
+                                Elem: &schema.Resource{
+                                        Schema: map[string]*schema.Schema{
+                                                "description": {
+                                                        Type:     schema.TypeString,
+                                                        Computed: true,
+                                                },
+                                                "id": {
+                                                        Type:     schema.TypeString,
+                                                        Computed: true,
+                                                },
+                                                "name": {
+                                                        Type:     schema.TypeString,
+                                                        Computed: true,
+                                                },
+                                                "source_type": {
+                                                        Type:     schema.TypeString,
+                                                        Computed: true,
+                                                },
+                                                "type": {
+                                                        Type:     schema.TypeString,
+                                                        Computed: true,
+                                                },
+                                        },
+                                },
+                        },
+                        "project_id": {
+                                Type:     schema.TypeString,
+                                Required: true,
+                                ForceNew: true,
+                        },
+                },
+        }
+}
+
+func resourceCatalogItemEntitlementCreate(d *schema.ResourceData, m interface{}) error {
+        log.Printf("starting to create vra_catalog_item_entitlement resource")
+
+        apiClient := m.(*Client).apiClient
+
+        catalogItemID := strfmt.UUID(d.Get("catalog_item_id").(string))
+
+        contentDefinition := models.ContentDefinition{
+                ID:   &catalogItemID,
+                Type: withString("CatalogItemIdentifier"),
+        }
+
+        entitlement := models.Entitlement{
+                Definition: &contentDefinition,
+                ProjectID:  withString(d.Get("project_id").(string)),
+        }
+
+        _, createResp, err := apiClient.CatalogEntitlements.CreateEntitlementUsingPOST(
+                catalog_entitlements.NewCreateEntitlementUsingPOSTParams().WithEntitlement(&entitlement))
+
+        if err != nil {
+                return err
+        }
+
+        d.SetId(createResp.GetPayload().ID.String())
+        log.Printf("Finished creating vra_catalog_item_entitlement resource with name %s", d.Get("name"))
+
+        return resourceCatalogItemEntitlementRead(d, m)
+}
+
+func resourceCatalogItemEntitlementRead(d *schema.ResourceData, m interface{}) error {
+        log.Printf("Reading the vra_catalog_item_entitlement resource with name %s", d.Get("name"))
+        apiClient := m.(*Client).apiClient
+
+        resp, err := apiClient.CatalogEntitlements.GetEntitlementsUsingGET(
+                catalog_entitlements.NewGetEntitlementsUsingGETParams().WithProjectID(withString(d.Get("project_id").(string))))
+
+        if err != nil {
+                return err
+        }
+
+        setFields := func(entitlement *models.Entitlement) {
+                d.SetId(entitlement.ID.String())
+                d.Set("project_id", entitlement.ProjectID)
+                d.Set("definition", flattenContentDefinition(entitlement.Definition))
+        }
+
+        if len(resp.Payload) > 0 {
+                for _, entitlement := range resp.Payload {
+                        if entitlement.Definition.ID.String() == d.Get("catalog_item_id").(string) {
+                                setFields(entitlement)
+                                log.Printf("Finished reading the vra_catalog_item_entitlement resource with name %s", d.Get("name"))
+                                return nil
+                        }
+                }
+        }
+
+        d.SetId("")
+        return nil
+}
+
+func resourceCatalogItemEntitlementDelete(d *schema.ResourceData, m interface{}) error {
+        log.Printf("Starting to delete the vra_catalog_item_entitlement resource with name %s", d.Get("name"))
+        apiClient := m.(*Client).apiClient
+
+        _, err := apiClient.CatalogEntitlements.DeleteEntitlementUsingDELETE(
+                catalog_entitlements.NewDeleteEntitlementUsingDELETEParams().WithID(strfmt.UUID(d.Id())))
+
+        if err != nil {
+                return err
+        }
+
+        d.SetId("")
+        log.Printf("Finished deleting the vra_catalog_item_entitlement resource with name %s", d.Get("name"))
+        return nil
+}
