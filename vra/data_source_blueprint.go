ae071839cd597313eaf9c252d7ebfd0a648166da
Ferran Rodenas <rodenasf@vmware.com>
2022-10-05 14:11:20 -0700

    Fix vra_blueprint datasource lookup by id
    * Update  attributes
    * Fix lookup logic by id
    
    Signed-off-by: Ferran Rodenas <rodenasf@vmware.com>

--- vra/data_source_blueprint.go
+++ vra/data_source_blueprint.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	"github.com/go-openapi/strfmt"
+
 	"github.com/vmware/vra-sdk-go/pkg/client/blueprint"
+	"github.com/vmware/vra-sdk-go/pkg/models"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/vmware/vra-sdk-go/pkg/models"
 )
 
 func dataSourceBlueprint() *schema.Resource {
@@ -16,102 +17,127 @@ func dataSourceBlueprint() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"content": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Cloud template YAML content.",
 			},
 			"content_source_id": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The id of the content source.",
 			},
 			"content_source_path": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The path of the content source.",
 			},
 			"content_source_sync_at": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Content source last sync at.",
 			},
 			"content_source_sync_messages": {
-				Type:     schema.TypeSet,
-				Computed: true,
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "Content source last sync status.",
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
 			},
 			"content_source_sync_status": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Content source last sync status.",
 			},
 			"content_source_type": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The type of the content source.",
 			},
 			"created_at": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date when the entity was created. The date is in ISO 6801 and UTC.",
 			},
 			"created_by": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The user the entity was created by.",
 			},
 			"description": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A human-friendly description.",
 			},
 			"id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"name"},
+				Description:   "The id of the cloud template.",
 			},
 			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"id"},
+				Description:   "The name of the cloud template.",
 			},
 			"org_id": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The id of the organization this entity belongs to.",
 			},
 			"project_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "The id of the project to narrow the search while looking for cloud templates.",
 			},
 			"project_name": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The name of the project the entity belongs to.",
 			},
 			"request_scope_org": {
 				Type:        schema.TypeBool,
 				Computed:    true,
-				Description: "Flag to indicate blueprint can be requested from any project in org",
+				Description: "Flag to indicate whether this cloud template can be requested from any project in the organization this entity belongs to.",
 			},
 			"self_link": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "HATEOAS of the entity.",
 			},
 			"status": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Status of the cloud template.",
 			},
 			"total_released_versions": {
-				Type:     schema.TypeInt,
-				Computed: true,
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Total number of released versions.",
 			},
 			"total_versions": {
-				Type:     schema.TypeInt,
-				Computed: true,
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Total number of versions.",
 			},
 			"updated_at": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date when the entity was last updated. The date is ISO 8601 and UTC.",
 			},
 			"updated_by": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The user the entity was last updated by.",
 			},
 			"valid": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Flag to indicate if the current content of the cloud template is valid.",
 			},
 			"validation_messages": {
 				Type:     schema.TypeSet,
@@ -119,27 +145,32 @@ func dataSourceBlueprint() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"message": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Validation message.",
 						},
 						"metadata": {
-							Type:     schema.TypeMap,
-							Optional: true,
+							Type:        schema.TypeMap,
+							Computed:    true,
+							Description: "Validation metadata.",
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
 						},
 						"path": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Validation path.",
 						},
 						"resource_name": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name of the resource.",
 						},
 						"type": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Message type.",
 						},
 					},
 				},
@@ -153,75 +184,71 @@ func dataSourceBlueprintRead(d *schema.ResourceData, meta interface{}) error {
 
 	id, idOk := d.GetOk("id")
 	name, nameOk := d.GetOk("name")
-	projectID, projectIDOk := d.GetOk("project_id")
 
 	if !idOk && !nameOk {
-		return fmt.Errorf("one of id or name must be assigned")
+		return fmt.Errorf("one of id or name is required")
 	}
 
-	var resp *blueprint.ListBlueprintsUsingGET1OK
-	var err error
-	projects := make([]string, 1)
+	var blueprintResource *models.Blueprint
+	if !idOk {
+		listBlueprintsUsingGET1Params := blueprint.NewListBlueprintsUsingGET1Params().WithName(withString(name.(string)))
+		if projectID, projectIDOk := d.GetOk("project_id"); projectIDOk {
+			listBlueprintsUsingGET1Params = listBlueprintsUsingGET1Params.WithProjects([]string{projectID.(string)})
+		}
+		getResp, err := apiClient.Blueprint.ListBlueprintsUsingGET1(listBlueprintsUsingGET1Params)
+		if err != nil {
+			return err
+		}
 
-	if projectIDOk {
-		projects = append(projects, projectID.(string))
-		resp, err = apiClient.Blueprint.ListBlueprintsUsingGET1(
-			blueprint.NewListBlueprintsUsingGET1Params().WithName(withString(name.(string))).WithProjects(projects))
-	} else {
-		resp, err = apiClient.Blueprint.ListBlueprintsUsingGET1(
-			blueprint.NewListBlueprintsUsingGET1Params().WithName(withString(name.(string))))
+		blueprints := getResp.GetPayload()
+		if len(blueprints.Content) > 1 {
+			return fmt.Errorf("more than one blueprint found with the same name, try to narrow filter by project_id")
+		}
+		if len(blueprints.Content) == 0 {
+			return fmt.Errorf("blueprint %s not found", name)
+		}
+
+		// ListBlueprintsUsingGET1Params does not return the blueprint content, so we need to use the GetBlueprintUsingGET1
+		// call in order to retrieve it.
+		id = blueprints.Content[0].ID
 	}
 
+	getResp, err := apiClient.Blueprint.GetBlueprintUsingGET1(blueprint.NewGetBlueprintUsingGET1Params().WithBlueprintID(strfmt.UUID(id.(string))))
 	if err != nil {
+		switch err.(type) {
+		case *blueprint.GetBlueprintUsingGET1NotFound:
+			return fmt.Errorf("blueprint '%s' not found", id)
+		default:
+			// nop
+		}
 		return err
 	}
 
-	if resp.GetPayload().NumberOfElements == 0 {
-		return fmt.Errorf("blueprint %s not found", name)
-	}
+	blueprintResource = getResp.GetPayload()
 
-	if resp.GetPayload().NumberOfElements > 1 {
-		return fmt.Errorf("more than one blueprint found with the same name, try to narrow filter by project_id")
-	}
+	d.SetId(blueprintResource.ID)
+	d.Set("content", blueprintResource.Content)
+	d.Set("content_source_id", blueprintResource.ContentSourceID)
+	d.Set("content_source_pat", blueprintResource.ContentSourcePath)
+	d.Set("content_source_sync_at", blueprintResource.ContentSourceSyncAt)
+	d.Set("content_source_sync_messages", blueprintResource.ContentSourceSyncMessages)
+	d.Set("content_source_sync_status", blueprintResource.ContentSourceSyncStatus)
+	d.Set("content_source_type", blueprintResource.ContentSourceType)
+	d.Set("created_at", blueprintResource.CreatedAt)
+	d.Set("created_by", blueprintResource.CreatedBy)
+	d.Set("description", blueprintResource.Description)
+	d.Set("name", blueprintResource.Name)
+	d.Set("org_id", blueprintResource.OrgID)
+	d.Set("project_id", blueprintResource.ProjectID)
+	d.Set("project_name", blueprintResource.ProjectName)
+	d.Set("request_scope_org", blueprintResource.RequestScopeOrg)
+	d.Set("self_link", blueprintResource.SelfLink)
+	d.Set("status", blueprintResource.Status)
+	d.Set("total_released_versions", blueprintResource.TotalReleasedVersions)
+	d.Set("total_versions", blueprintResource.TotalVersions)
+	d.Set("updated_at", blueprintResource.UpdatedAt)
+	d.Set("updated_by", blueprintResource.UpdatedBy)
+	d.Set("valid", blueprintResource.Valid)
 
-	setFields := func(blueprint *models.Blueprint) {
-		d.SetId(blueprint.ID)
-		d.Set("content", blueprint.Content)
-		d.Set("content_source_id", blueprint.ContentSourceID)
-		d.Set("content_source_pat", blueprint.ContentSourcePath)
-		d.Set("content_source_sync_at", blueprint.ContentSourceSyncAt)
-		d.Set("content_source_sync_messages", blueprint.ContentSourceSyncMessages)
-		d.Set("content_source_sync_status", blueprint.ContentSourceSyncStatus)
-		d.Set("content_source_type", blueprint.ContentSourceType)
-		d.Set("created_at", blueprint.CreatedAt)
-		d.Set("created_by", blueprint.CreatedBy)
-		d.Set("description", blueprint.Description)
-		d.Set("name", blueprint.Name)
-		d.Set("org_id", blueprint.OrgID)
-		d.Set("project_id", blueprint.ProjectID)
-		d.Set("project_name", blueprint.ProjectName)
-		d.Set("request_scope_org", blueprint.RequestScopeOrg)
-		d.Set("self_link", blueprint.SelfLink)
-		d.Set("status", blueprint.Status)
-		d.Set("total_released_versions", blueprint.TotalReleasedVersions)
-		d.Set("total_versions", blueprint.TotalVersions)
-		d.Set("updated_at", blueprint.UpdatedAt)
-		d.Set("updated_by", blueprint.UpdatedBy)
-		d.Set("valid", blueprint.Valid)
-	}
-
-	for _, bp := range resp.Payload.Content {
-		if (idOk && bp.ID == id) || (nameOk && bp.Name == name.(string)) {
-			bpDetails, err := apiClient.Blueprint.GetBlueprintUsingGET1(
-				blueprint.NewGetBlueprintUsingGET1Params().WithBlueprintID(strfmt.UUID(bp.ID)))
-			if err != nil {
-				return err
-			}
-			setFields(bpDetails.GetPayload())
-
-			return nil
-		}
-	}
-
-	return fmt.Errorf("blueprint %s not found", name)
+	return nil
 }
